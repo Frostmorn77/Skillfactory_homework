2ce5c9abd12090adbe532514cbd5324a3c4d95d4
Frostmorn77 <veil17987@gmail.com>
2023-04-22 13:39:28 +0300

    Work 27.7.2

---
+++ work_27.7.2/PetFriendsTest/settings.py
@@ -0,0 +1,7 @@
+
+valid_email = ""
+valid_password = ""
+not_valid_email = "gorc28371928@yandex.ru"
+not_valid_password = "sndfbjfbwvclsldmc"
+lost_email = ""
+lost_password = ""
---
+++ work_27.7.2/calc_tests/calc_avto_tests/tests.py
@@ -0,0 +1,19 @@
+import pytest
+
+from app.calc import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_adding_success(self):
+        assert self.calc.adding(self, 1, 2) == 3
+
+    def test_subtraction_success(self):
+        assert self.calc.subtraction(self, 5, 2) == 3
+
+    def test_division_success(self):
+        assert self.calc.division(self, 10, 2) == 5
+
+    def test_multiply_success(self):
+        assert self.calc.multiply(self, 3, 3) == 9
